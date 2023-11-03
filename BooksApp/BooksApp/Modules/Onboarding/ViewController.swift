77d5f31303c68c8ab4c5267566a3883b7a595d41
YuliaSimanavets <mrs.simonovets@gmail.com>
2023-11-03 13:34:41 +0100

    added extensions [UIColor, String]

--- BooksApp/BooksApp/Modules/Onboarding/ViewController.swift
+++ BooksApp/BooksApp/Modules/Onboarding/ViewController.swift
@@ -11,7 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemYellow
+        view.backgroundColor = UIColor.Primary.background
+        
+    
     }
 }
 

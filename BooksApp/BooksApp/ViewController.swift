c8031316b88c70f0d3f8d3a676e804e123a6da38
YuliaSimanavets <mrs.simonovets@gmail.com>
2023-11-03 13:18:43 +0100

    created xcode project

---
+++ BooksApp/BooksApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  BooksApp
+//
+//  Created by Yuliya on 03/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+}
+
